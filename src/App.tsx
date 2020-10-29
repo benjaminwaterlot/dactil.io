f7128e37b8603b7c153ef28eb513dba2f43dd9b0
Benjamin Waterlot <waterlotbenjamin@gmail.com>
2020-10-29 13:26:00 +0100

    feat: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  Box,
+  Flex,
+  Button,
+  Heading,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverContent,
+  PopoverTrigger,
+  Text,
+  useColorMode,
+} from '@chakra-ui/core'
 
 function App() {
+  const { toggleColorMode } = useColorMode()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Flex as="header" py={12} px={6} direction="column" align="center">
+        <Heading size="2xl" as="h1">
+          Dactil.io
+        </Heading>
+
+        <Box mt={200} p={10} bg="gray.900" borderRadius={5} w={['100%', '50%']} textAlign="center">
+          <Text fontFamily="mono" fontSize="lg" my={5} color="gray.600">
+            The spectacle before us was indeed sublime.
+          </Text>
+          <Flex minH="60px" d="flex" flexDirection="column" justifyContent="center">
+            <Text fontFamily="mono" fontSize="xl">
+              <Box as="span" bg="gray.200" color="gray.800">
+                T
+              </Box>
+              he spectacle before us was indeed sublime.
+            </Text>
+          </Flex>
+          <Text fontFamily="mono" fontSize="lg" my={5} color="gray.600">
+            The spectacle before us was indeed sublime.
+          </Text>
+        </Box>
+
+        <Popover trigger="hover">
+          <PopoverTrigger>
+            <Text mt={200}>Test my popover!</Text>
+          </PopoverTrigger>
+
+          <PopoverContent>
+            <PopoverArrow />
+            <PopoverBody>Heyyy</PopoverBody>
+          </PopoverContent>
+        </Popover>
+
+        <Button onClick={toggleColorMode} mt={20}>
+          Toggle color
+        </Button>
+      </Flex>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { ChakraProvider, CSSReset, extendTheme } from '@chakra-ui/core'
+
+const customTheme = extendTheme({
+  config: { initialColorMode: 'dark', useSystemColorMode: false },
+  fonts: {
+    heading: 'Merriweather',
+  },
+})
+
+console.log({ theme: customTheme })
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={customTheme}>
+      <CSSReset />
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
